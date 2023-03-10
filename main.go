227c9ece76680ccddd16d458ef48a80b178d381b
dhew56 <127263127+dhew56@users.noreply.github.com>
2023-03-11 06:45:26 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"dts-go-07/photo"
-	"dts-go-07/user"
 	"fmt"
 )
 
@@ -50,43 +48,4 @@ func main() {
 
 	//menampilkan float scientific : 1.234560e+02
 	fmt.Printf("%e \n", k)
-
-	// MINI QUIZ
-	// budi adalah user bertipe admin
-	// dia memiliki foto sebanyak 10 foto
-	// buatlah program yang akan menghasilkan output
-	// 1. jika user adalah admin dan foto > 10 => akan print "ok"
-	// 2. jika user adalah admin dan foto >= 10 => akan print "tidak ok"
-	// 3. selain diatas => akan print "oke gak ya?"
-	// kriteria:
-	// type user disimpan di package user
-	// batas foto disimpan di package photo
-	// program akan dijalankan di main.go
-
-	pengguna := 0
-	if pengguna == user.ADMIN {
-		fmt.Println("pengguna adalah ADMIN")
-	} else if pengguna == user.NORMAL {
-		fmt.Println("pengguna adalah NORMAL")
-	} else if pengguna == user.VIP {
-		fmt.Println("pengguna adalah VIP")
-	}
-
-	budiType := 0 // admin 0
-	budiPhoto := 10
-
-	// give white space
-	fmt.Println()
-
-	// budiType == user.ADMIN => true
-	// budiPhoto > photo.BATAS_FOTO => false
-	// budiPhoto >= photo.BATAS_FOTO => true
-
-	if budiType == user.ADMIN && budiPhoto > photo.BATAS_FOTO {
-		fmt.Println("ok")
-	} else if budiType == user.ADMIN && budiPhoto >= photo.BATAS_FOTO {
-		fmt.Println("tidak ok")
-	} else {
-		fmt.Println("oke gak ya?")
-	}
 }
