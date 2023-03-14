095130b7388969f43d3e6742adc607fa0223e287
dhew56 <127263127+dhew56@users.noreply.github.com>
2023-03-14 13:00:37 +0700

    Add files via upload

--- main.go
+++ main.go
@@ -1,51 +1,23 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func main() {
+func main(){
 
-	//CHALLENGE 01-Dhewangga Arie-Kelas 07//
-
-	//menampilkan nilai i : 21
-	var i int = 21
-	fmt.Printf("%d \n", i)
-
-	//menampilkan tipe data dari variabel i
-	fmt.Printf("%T \n", i)
-
-	//menampilkan tanda %
-	fmt.Println("%")
-
-	//menampilkan nilai boolean j : true
-	var j bool = true
-	fmt.Printf("%t \n", j)
-
-	//menampilkan unicode russia : Я (ya)
-	cyrillic := 'Я'
-	fmt.Printf("%c \n", cyrillic)
-
-	//menampilkan nilai base 10 : 21
-	fmt.Printf("%d \n", i)
-
-	//menampilkan nilai base 8 :25
-	fmt.Printf("%o \n", i)
-
-	//menampilkan nilai base 16 : f
-	output := 15
-	fmt.Printf("%x \n", output)
-
-	//menampilkan nilai base 16 : F 13
-	fmt.Printf("%X 13 \n", output)
-
-	//menampilkan unicode karakter Я : U+042F
-	fmt.Printf("%U \n", cyrillic)
-
-	//menampilkan float : 123.456000
-	var k float64 = 123.456
-	fmt.Printf("%f \n", k)
-
-	//menampilkan float scientific : 1.234560e+02
-	fmt.Printf("%e \n", k)
+	for i := 0; i <= 5; i++ {
+		fmt.Printf("Nilai i = %d\n", i) 
+		if i == 5 {
+			for j := 0; j <= 10; j++ {
+				if j == 5 {
+					cyrillic := "САШАРВО"
+					for index, value := range cyrillic {
+						fmt.Printf("character %U, %c, starts at byte position %d\n", value, value, index)
+					}
+				} else {
+					fmt.Printf("Nilai j = %d\n", j)
+				}
+			}
+		} 
+	}
+	
 }
